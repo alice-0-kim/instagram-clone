2b9fe33b025a26d7726dec9f80645b897824b07c
Alice Kim <aliceykim0828@gmail.com>
2020-08-26 11:25:42 -0700

    lint

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,19 +1,19 @@
 import React, { useRef } from 'react'
-import Layout from '../components/Layout'
 import { Button, Input } from '@material-ui/core'
+import Layout from '../components/Layout'
 
 const Home = () => {
     const Image = useRef(null)
     const handleClick = () => {
         console.log(Image.current?.value)
     }
-    const HomePage = () => {
-        return <Layout>
+    const HomePage = () => (
+        <Layout>
             <h1>Home page</h1>
             <Input inputRef={Image} fullWidth />
             <Button variant="outlined" onClick={handleClick} style={{ margin: '1rem auto' }}>Upload Image</Button>
         </Layout>
-    }
+    )
     return <HomePage />
 }
 
