473ccbcce947c2a95fd5854d96a81dd1c2c81741
Alice <aliceykim0828@gmail.com>
2020-08-27 18:23:23 -0700

    Create README.md (#1)
    * ignore JSON key file
    
    * update README.md
    
    * lint
    
    * update username

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,19 +1,17 @@
 import React from 'react'
 
-const Layout = ({ children }) => {
-    return (
-        <>
-            <div
-                style={{
-                    margin: `0 auto`,
-                    maxWidth: 960,
-                    padding: `0 1.0875rem 1.45rem`,
-                }}
-            >
-                <main>{children}</main>
-            </div>
-        </>
-    )
-}
+const Layout = ({ children }) => (
+    <>
+        <div
+            style={{
+                margin: `0 auto`,
+                maxWidth: 960,
+                padding: `0 1.0875rem 1.45rem`,
+            }}
+        >
+            <main>{children}</main>
+        </div>
+    </>
+)
 
 export default Layout
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 
 const Profile = () => {
     const user = {
-        username: 'alice-0-kim',
+        username: 'yehee',
         posts: 10,
         followers: 14,
         following: 4,
@@ -18,37 +18,47 @@ const Profile = () => {
         { url: 'https://images.unsplash.com/photo-1519052537078-e6302a4968d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80' },
     ]
     const ProfilePage = () => {
-        const { username, posts, followers, following, short } = user
-        return <Layout>
-            <div style={{ display: 'flex', margin: '5rem auto', alignItems: 'center' }}>
-                <img
-                    src={'https://images.unsplash.com/photo-1519052537078-e6302a4968d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'}
-                    style={{
-                        width: 100,
-                        height: 100,
-                        borderRadius: '50%',
-                        objectFit: 'cover',
-                        margin: '0 3rem',
-                    }}
-                />
-                <div>
-                    <p>{username}</p>
-                    <div style={{ fontSize: 'small' }}>
-                        <p>
-                            <span>{`${posts} posts `}</span>
-                            <span>{`${followers} followers `}</span>
-                            <span>{`${following} following`}</span>
-                        </p>
-                        <p>{short}</p>
+        const {
+            username, posts, followers, following, short,
+        } = user
+        return (
+            <Layout>
+                <div style={{ display: 'flex', margin: '5rem auto', alignItems: 'center' }}>
+                    <img
+                        src="https://images.unsplash.com/photo-1519052537078-e6302a4968d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+                        style={{
+                            width: 100,
+                            height: 100,
+                            borderRadius: '50%',
+                            objectFit: 'cover',
+                            margin: '0 3rem',
+                        }}
+                    />
+                    <div>
+                        <p>{username}</p>
+                        <div style={{ fontSize: 'small' }}>
+                            <p>
+                                <span>{`${posts} posts `}</span>
+                                <span>{`${followers} followers `}</span>
+                                <span>{`${following} following`}</span>
+                            </p>
+                            <p>{short}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div>
-                {photos.map(({ url }, i) => {
-                    return <img key={i} src={url} style={{ width: 310, height: 310, margin: 5, objectFit: 'cover' }} />
-                })}
-            </div>
-        </Layout>
+                <div>
+                    {photos.map(({ url }, i) => (
+                        <img
+                            key={i}
+                            src={url}
+                            style={{
+                                width: 310, height: 310, margin: 5, objectFit: 'cover',
+                            }}
+                        />
+                    ))}
+                </div>
+            </Layout>
+        )
     }
     return <ProfilePage />
 }
